485061c2091b7600818acffc8d7538932153a206
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-09-22 18:59:08 +0300

    Delete problema_6.py

--- problema_6.py
+++
@@ -1,7 +0,0 @@
-n=1245
-print('ultima cifra', n%10)
-print('penultima cifra', n%100//10)
-print('restul n/9=', n%9, 'catul n/9=', n//9)
-print('suma cifrelor=', n%10+n%100//10+n%1000//100+n%10000//1000)
-print('rasturnatul', n%10,n%100//10,n%1000//100,n%10000//1000)
-
