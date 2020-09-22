8fe23db811e740ba67610759bf4b44e27d00f8e9
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-09-22 19:04:00 +0300

    Add files via upload

---
+++ problema_1.py
@@ -0,0 +1,7 @@
+print\
+     ("""    struguri                100              kg
+    mere                    10               tone
+    cartofi                 250              kg
+    varza                   1000             q"""
+    ) 
+    
